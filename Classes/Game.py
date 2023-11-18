e061c84e7662b46821d17eb2edd54c4f58134815
hugopuybareau <151200796+hugopuybareau@users.noreply.github.com>
2023-11-19 00:25:15 +0100

    commit game

--- Classes/Game.py
+++ Classes/Game.py
@@ -1,9 +1,18 @@
 class Game :
-    def __init__(self, username : str, players_name : list, hand : list) :
+    def __init__(self, username : str, players_name : list, hand : list, prize_bb : float, pot_bb = float) :
         self.username = username
         self.players_name = players_name
         self.hand = hand
+<<<<<<< Updated upstream
         self.cards = [None] * 5
+=======
+        self.cards = None
+        self.prize_bb = None
+        self.pot_bb = None
+        
+
+        
+>>>>>>> Stashed changes
 
     #TODO
     def updatePlayers(self) :
