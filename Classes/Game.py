cf329d5836308e8d486a9363abde9ef5fb2cd71f
Rossignol Maxence <maxence.rossignol@student-cs.fr>
2023-11-19 00:06:14 +0100

    Ajout classe Game et main

---
+++ Classes/Game.py
@@ -0,0 +1,5 @@
+class Game :
+    def __init__(self, players_name : list, hand : list, position : int) :
+        self.players_name = players_name
+        self.hand = hand
+        self.position = position
